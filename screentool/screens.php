d1410ef7744ef51beb2b8e01b3cd23960a1d9ae8
Michał <38110165+Michal2SAB@users.noreply.github.com>
2018-04-07 18:06:43 +0200

    Create screens.php
    A tool that saves our screenshot

---
+++ screentool/screens.php
@@ -0,0 +1,37 @@
+<?php
+$secure_connection = true;
+if(isset($_SERVER['HTTPS'])) {
+$servername = "DATABASE_SERVER";
+$username = "DATABASE_USERNAME";
+$password = "DATABASE_PASSWORD";
+$dbname = "hydraant_anticheat";
+
+
+$con = new mysqli($servername, $username, $password, $dbname);
+
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+} 
+echo "Connected successfully";
+
+// Temporary file name stored on the server
+date_default_timezone_set('Europe/Warsaw');
+$name=$_POST['user'];
+$date = date('Y-m-d G:i:s', time());
+$ip = $_SERVER['REMOTE_ADDR'];
+$tmpName = "images/".$name.".png";
+// Read the file
+$fp = fopen($tmpName, 'r');
+$data = fread($fp, filesize($tmpName));
+$data = addslashes($data);
+fclose($fp);
+
+
+$query = "INSERT INTO screens ";
+$query .= "(screen,username,date,ip) VALUES ('$data','$name','$date','$ip')";
+$results = mysqli_query($con, $query);
+
+// Close our MySQL Link
+mysqli_close($con);
+}
+?>
